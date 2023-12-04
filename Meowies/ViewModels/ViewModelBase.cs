908e4f8cc1cac06e94b432b6c9e0a4ba98763602
Valerie <valeria_serebrennikova@mail.ru>
2023-12-04 16:30:46 +0300

    initial commit

---
+++ Meowies/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Meowies.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Meowies/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Meowies.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
